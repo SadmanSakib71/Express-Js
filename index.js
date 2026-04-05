b464b4fd5a90dd0e2be4f02ac1d6e24e57965445
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-04-05 07:16:12 +0600

    express.urlenCodded() is function get data like object

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.use(express.text());
+app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
   res.send("this is get for home page");
