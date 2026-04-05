41bebaeaee862f101cdd6d2de3fb1ffb445fe545
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-04-05 07:07:57 +0600

    express.raw() is a function who return beffuer data,and we have to string it to get the data

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json());
+app.use(express.raw());
 
 app.get("/", (req, res) => {
   res.send("this is get for home page");
 });
 
 app.post("/", (req, res) => {
-  console.log(req.body);
+  console.log(req.body.toString());
 
   res.send("this is post for my home page");
 });
