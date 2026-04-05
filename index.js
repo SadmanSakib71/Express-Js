52f4b476e4b6e516e5997d33f5f85d34e1ed374c
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-04-05 10:13:58 +0600

    for same route

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const app = express();
 
-app.param("id", (req, res, next, id) => {
-  const user = {
-    userId: id,
-    name: "bangladesh",
-  };
-  req.userDetails = user;
-  next();
-});
-
-app.get("/about/:id", (req, res) => {
-  console.log(req.userDetails);
-
-  res.send("this is post for my home page");
-});
+app
+  .route("/sakib/akif")
+  .get("", (req, res) => {
+    res.send("this is post for my home page");
+  })
+  .post("", (req, res) => {
+    res.send("this is post for my home page");
+  })
+  .put("", (req, res) => {
+    res.send("this is post for my home page");
+  })
+  .delete("", (req, res) => {
+    res.send("this is post for my home page");
+  });
 
 app.listen(3000, () => {
   console.log("listening on the server port 3000");
