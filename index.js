e9361052f08ad80cabce99cf0b7ef7b489feb09b
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-04-06 05:49:23 +0600

    know the baseUrl

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 const express = require("express");
 const app = express();
+const adminRoute = express.Router();
 
-app.set("view engine", "ejs");
+adminRoute.get("/dashboard", (req, res) => {
+  console.log(req.baseUrl);
 
-app
-  .route("/sakib/akif")
-  .get((req, res) => {
-    res.render("pages/page");
-  })
-  .post((req, res) => {
-    res.send("this is post for my home page");
-  })
-  .put((req, res) => {
-    res.send("this is post for my home page");
-  })
-  .delete((req, res) => {
-    res.send("this is post for my home page");
-  });
+  res.send("we are in admin route");
+});
+
+app.use("/admin", adminRoute);
+
+app.get("/user/:id", (req, res) => {
+  console.log(req.baseUrl);
+  res.send("this is for get method");
+});
 
 app.listen(3000, () => {
   console.log("listening on the server port 3000");
