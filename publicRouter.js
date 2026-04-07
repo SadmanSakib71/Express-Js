aadb1afdad28e4f8d0425d0382ebf4a453b1cb7f
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-04-07 06:12:06 +0600

    router.all method knows

--- publicRouter.js
+++ publicRouter.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const publicRouter = express.Router();
 
+const log = (req, res, next) => {
+  console.log("i am logging something in my home");
+  next();
+};
+
+publicRouter.all("{*splat}", log);
+
 publicRouter.get("/", (req, res) => {
-  res.send("for home router");
+  res.send("for home ");
+});
+publicRouter.get("/about", (req, res) => {
+  res.send("for home about ");
 });
 
 module.exports = publicRouter;
